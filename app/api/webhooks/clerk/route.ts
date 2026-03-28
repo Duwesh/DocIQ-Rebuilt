a95162ffd2863cfebc61410f95a2fc82702e7a81
Duwesh Kumar <67849097+Duwesh@users.noreply.github.com>
2026-03-28 23:53:22 +0530

    feat: configure prisma and implement clerk webhooks for user and org sync

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,69 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error("Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
+  }
+
+  const headerPayload = headers();
+  const svix_id = (await headerPayload).get("svix-id");
+  const svix_timestamp = (await headerPayload).get("svix-timestamp");
+  const svix_signature = (await headerPayload).get("svix-signature");
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occurred -- no svix headers", {
+      status: 400,
+    });
+  }
+
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: WebhookEvent;
+
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    return new Response("Error occurred -- verification failed", {
+      status: 400,
+    });
+  }
+
+  const { id } = evt.data;
+  const eventType = evt.type;
+
+  if (eventType === "user.created") {
+    const { id, email_addresses, first_name, last_name } = evt.data;
+    await db.user.create({
+      data: {
+        clerkUserId: id,
+        email: email_addresses[0].email_address,
+        name: `${first_name} ${last_name}`,
+      },
+    });
+  }
+
+  if (eventType === "organization.created") {
+     const { id, name, slug } = evt.data;
+     await db.organization.create({
+       data: {
+         clerkOrgId: id,
+         name,
+         slug,
+       }
+     });
+  }
+
+  return new Response("", { status: 200 });
+}
